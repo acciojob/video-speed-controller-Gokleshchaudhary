6bad2fbc2e588a3bfce068ebe10b029dea0cb5bb
Gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-06-12 11:18:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('video');
+const playButton = document.querySelector('.player__button');
+const progressFilled = document.querySelector('.progress__filled');
+const volumeInput = document.querySelector('.volume');
+const speedInput = document.querySelector('.playbackSpeed');
+const rewindButton = document.querySelector('.rewind');
+const skipButton = document.querySelector('.skip');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+playButton.addEventListener('click', () => {
+    if (video.paused) {
+        video.play();
+        playButton.textContent = '❚ ❚'; // Change to pause symbol
+    } else {
+        video.pause();
+        playButton.textContent = '►'; // Change to play symbol
     }
+});
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+video.addEventListener('timeupdate', () => {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressFilled.style.width = `${percent}%`;
+});
+
+volumeInput.addEventListener('input', (e) => {
+    video.volume = e.target.value;
+});
+
+speedInput.addEventListener('input', (e) => {
+    video.playbackRate = e.target.value;
+});
+
+rewindButton.addEventListener('click', () => {
+    video.currentTime -= 10;
+});
+
+skipButton.addEventListener('click', () => {
+    video.currentTime += 25;
+});
